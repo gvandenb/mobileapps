e62332480e0087f253ff7d81006ccd423152f2fd
Greg Vandenberg <gvandenb@fame.me>
2013-01-02 13:13:44 -0800

    update for mpp

--- PharmacyPal/www/js/index.js
+++ PharmacyPal/www/js/index.js
@@ -1,3 +1,6 @@
+var SERVER_PROTOCOL = "http://";
+var SERVER_JSON = "dev.mypharmacypal.com";
+var userPref = new userPreference();
 var prescriptionNum = null;
 var app = {
     initialize: function() {
@@ -9,34 +12,15 @@ var app = {
     deviceready: function() {
     	console.log("Device Ready");
     	
-    	/*plugins.navigationBar.init();
-
-        plugins.navigationBar.create(); // or .create("BlackOpaque") to apply a certain style
-        plugins.navigationBar.hideLeftButton();
-        plugins.navigationBar.hideRightButton();
-
-        plugins.navigationBar.setTitle("My heading");
-
-        plugins.navigationBar.showLeftButton();
-        plugins.navigationBar.showRightButton();
-
-        // Create left navigation button with a title (you can either have a title or an image, not both!)
-        plugins.navigationBar.setupLeftButton("Text", null, function() {
-            alert("left nav button tapped");
-        });
-
-        // Create right navigation button from a system-predefined button (see the full list in NativeControls.m)
-        // or from an image
-        plugins.navigationBar.setupRightButton(
-            null,
-            "barButton:Bookmarks", // or your own file like "/www/stylesheets/images/ajax-loader.png",
-            function() {
-                alert("right nav button tapped");
-            }
-        );
-
-        plugins.navigationBar.show();*/
-    	
+    
+		console.log("in redir");
+		if (userPref.userid == null) {
+			$.mobile.changePage( "signin.html", { transition: "fade"} );
+		}
+		else {
+			$.mobile.changePage( "home.html", { transition: "fade"} );	
+		}
+		
  	    $('#scanBarcodePage').live('pageshow', function(event) {   //Workaround to show page loading on initial page load	
  	    	console.log("Displaying scan page");
  	    	$('#scanBarcode').click(function(){
@@ -66,6 +50,14 @@ var app = {
  			   prescriptionNum = null;
  		   }
  	   });
+ 	   
+ 	   $('#register').live('pageshow', function(event) {   //Workaround to show page loading on initial page load	
+ 		   createUser();
+	   });
+	    
+	   $('#signin').live('pageshow', function(event) {   //Workaround to show page loading on initial page load	
+		   signIn();
+	   });
     	// note that this is an event handler so the scope is that of the event
         // so we need to call app.report(), and not this.report()
         initPushwoosh();
@@ -116,7 +108,178 @@ var app = {
     
 };
 
+function userPreference() {
+	this.userid = null;
+	this.license = 0;
+	this.location = 0;
+	this.tutorial = 0;
+	this.push = 0;
+	this.sms = 0;
+}
 
+function onCreateUserSuccess(data, status)
+{
+	console.log("Account created successfully");
+	$.mobile.hidePageLoadingMsg();
+	
+	console.log(data);
+	
+	userPref.userid = data.userid;
+	console.log(data.userid);
+	// put in local storage
+  	localStorage.setItem("userPref", JSON.stringify(userPref));
+  	console.log("set local storage");
+  	if (userPref.tutorial == 0) {
+    	$.mobile.changePage( "tutorial.html", { transition: "slide"} );
+    }
+    else {
+    	console.log("changing to home.html");
+    	$.mobile.changePage( "home.html", { transition: "slide"} );
+    }
+    
+}
+
+function onCreateUserError(data, status)
+{
+    // handle an error
+	$.mobile.hidePageLoadingMsg();
+	
+	var e = JSON.parse(data.responseText);
+	console.log(data);
+	// reset error messages
+	$('#emailError').html("");
+	$('#passwordError').html("");
+	$('#confirmError').html("");
+	$('#firstnameError').html("");
+	$('#lastnameError').html("");
+	
+	if (e.User_email) {
+		var message = JSON.stringify(e.User_email);
+		$('#emailError').html(message);
+	}
+	if (e.User_password) {
+		var message = JSON.stringify(e.User_password);
+		$('#passwordError').html(message);
+	}
+	if (e.User_confirm) {
+		var message = JSON.stringify(e.User_confirm);
+		$('#confirmError').html(message);
+	}
+	if (e.User_firstname) {
+		var message = JSON.stringify(e.User_firstname);
+		$('#firstnameError').html(message);
+	}
+	if (e.User_lastname) {
+		var message = JSON.stringify(e.User_lastname);
+		$('#lastnameError').html(message);
+	}
+	 
+}        
+
+function onSigninSuccess(data, status)
+{
+	$.mobile.hidePageLoadingMsg();
+	console.log(data.userid);
+	userPref.userid = data.userid;
+	$.mobile.showPageLoadingMsg();
+	$.ajax({
+		type: 'POST',
+		data: encodeURIComponent('SignIn[json]')+ '='+JSON.stringify(userPref),
+		url: 'http://' + SERVER_JSON + '/user/serviceJSON',
+		dataType: 'json',
+		success: function(data){
+			console.log(data.status_message);
+			 $.mobile.hidePageLoadingMsg();
+			 console.log("Services updated");
+		},
+		error: function(){
+			console.log(data.status_message);
+			$.mobile.hidePageLoadingMsg();
+			//alert('There was an error adding your message');
+		}
+	});	
+	
+	// update local storage
+	 rawData = localStorage.getItem("userPref");
+     // Data parsed here as it was stringified before saving
+     tempUserPref = JSON.parse(rawData);
+     tempUserPref.userid = data.userid;
+     userPref = tempUserPref;
+     localStorage.setItem("userPref", JSON.stringify(userPref));
+     //console.log("userPref.tutorial: " + userPref.tutorial);
+	if (userPref.tutorial == 0) {
+		$.mobile.changePage( "tutorial.html", { transition: "slide"} );
+	}
+	else {
+		$.mobile.changePage( "home.html", { transition: "slide"} );		
+	}
+    
+}
+
+function onSigninError(data, status)
+{
+    // handle an error
+	$.mobile.hidePageLoadingMsg();
+	
+	var e = $.parseJSON(data.responseText);
+	
+	// reset error messages
+	$('#usernameError').html("");
+	$('#passwordError').html("");
+	
+	if (e.LoginForm_username) {
+		var message = JSON.stringify(e.LoginForm_username);
+		$('#usernameError').html(message);
+	}
+	if (e.LoginForm_password) {
+		var message = JSON.stringify(e.LoginForm_password);
+		$('#passwordError').html(message);
+	}
+
+	 
+}
+
+function createUser() {
+	$("#registerSubmit").click(function(){
+        var formData = $("#createUser").serialize();
+
+        $.mobile.showPageLoadingMsg();
+        $.ajax({
+            type: "POST",
+            url: "http://" + SERVER_JSON + "/user/create",
+            cache: false,
+            data: formData,
+            dataType: 'json',
+            success: onCreateUserSuccess,
+            error: onCreateUserError
+        });
+
+        return false;
+    });
+	
+
+}
+
+function signIn() {
+	$("#signinSubmit").click(function(){
+        var formData = $("#signIn").serialize();
+
+        $.mobile.showPageLoadingMsg();
+        $.ajax({
+            type: "POST",
+            url: "http://" + SERVER_JSON + "/site/login",
+            cache: false,
+            data: formData,
+            dataType: 'json',
+            success: onSigninSuccess,
+            error: onSigninError
+        });
+
+        return false;
+    });
+	
+
+}
 function initPushwoosh()
 {
 	var pushNotification = window.plugins.pushNotification;
